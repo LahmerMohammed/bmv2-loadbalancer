13ff8e1a47d29184dffea847ec62ad9f3340b7a2
Lahmer Mohammed <lahmermohammed65@gmail.com>
2023-04-27 18:16:17 +0100

    added missing libs

--- yolo-service/run.sh
+++ yolo-service/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update > /dev/null
-apt install python3-pip python3-venv libglvnd-dev mesa-utils -y
+apt install python3-pip python3-venv libglvnd-dev mesa-utils ffmpeg libsm6 libxext6 -y 
 
 
 python3 -m venv venv
