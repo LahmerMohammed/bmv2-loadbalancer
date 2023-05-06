e4ff74972b89eeaf7d1e5bb2beb040fd0ffb3b04
Lahmer Mohammed <lahmermohammed65@gmail.com>
2023-05-06 20:54:10 +0100

    update yolo model

--- ml_model/yolo/api/run.sh
+++ ml_model/yolo/api/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-current_dir=$(pwd)
-
-
 apt update > /dev/null
 apt install python3-pip python3-venv libglvnd-dev mesa-utils ffmpeg libsm6 libxext6 -y 
 
@@ -12,21 +9,15 @@ python3 -m venv venv
 
 pip3 install -r requirements.txt
 
-
-# Create directory if it does not exist
-if [ ! -d "/root/.cvlib/object_detection/yolo/yolov3" ]; then
-  mkdir -p /root/.cvlib/object_detection/yolo/yolov3
-fi
+mkdir -p /root/.cvlib/object_detection/yolo/yolov3
 
 yolo_path=/root/.cvlib/object_detection/yolo/yolov3
 
 # Download files
-wget https://pjreddie.com/media/files/yolov3.weights -O ${yolo_path}/yolov3.weights &
-wget https://github.com/pjreddie/darknet/raw/master/cfg/yolov3.cfg -O ${yolo_path}/yolov3.cfg &
-wget https://github.com/arunponnusamy/object-detection-opencv/raw/master/yolov3.txt -O ${yolo_path}/yolov3.txt &
+wget https://pjreddie.com/media/files/yolov3.weights -O ${yolo_path}/yolov3.weights
+wget https://github.com/pjreddie/darknet/raw/master/cfg/yolov3.cfg -O ${yolo_path}/yolov3.cfg
+wget https://github.com/arunponnusamy/object-detection-opencv/raw/master/yolov3.txt -O ${yolo_path}/yolov3.txt
 
-# Wait for all downloads to complete
-wait
 
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
 
