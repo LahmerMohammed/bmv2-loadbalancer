bf387e3a682209ab92e973e40d88100f7ac0caa4
Lahmer Mohammed <lahmermohammed65@gmail.com>
2023-05-13 23:44:38 +0100

    updated run.sh

--- ml_model/yolo/api/run.sh
+++ ml_model/yolo/api/run.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-#apt update > /dev/null
-#apt install python3-pip python3-venv libglvnd-dev mesa-utils ffmpeg libsm6 libxext6 -y 
+apt update > /dev/null
+apt install python3-pip python3-venv libglvnd-dev mesa-utils ffmpeg libsm6 libxext6 -y 
 
 
-#python3 -m venv venv
-#. venv/bin/activate
+python3 -m venv venv
+. venv/bin/activate
 
-#pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 
-#mkdir -p /root/.cvlib/object_detection/yolo/yolov3
+mkdir -p /root/.cvlib/object_detection/yolo/yolov3
 
-#yolo_path=/root/.cvlib/object_detection/yolo/yolov3
+yolo_path=/root/.cvlib/object_detection/yolo/yolov3
 
 # Download files
-#wget https://pjreddie.com/media/files/yolov3.weights -O ${yolo_path}/yolov3.weights
-#wget https://github.com/pjreddie/darknet/raw/master/cfg/yolov3.cfg -O ${yolo_path}/yolov3.cfg
-#wget https://github.com/arunponnusamy/object-detection-opencv/raw/master/yolov3.txt -O ${yolo_path}/yolov3.txt
+wget https://pjreddie.com/media/files/yolov3.weights -O ${yolo_path}/yolov3.weights
+wget https://github.com/pjreddie/darknet/raw/master/cfg/yolov3.cfg -O ${yolo_path}/yolov3.cfg
+wget https://github.com/arunponnusamy/object-detection-opencv/raw/master/yolov3.txt -O ${yolo_path}/yolov3.txt
+
 
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
 
