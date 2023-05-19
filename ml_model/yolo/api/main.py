abe9016627ef860fb760f4565e14747a5933da58
Lahmer Mohammed <lahmermohammed65@gmail.com>
2023-05-19 22:16:45 +0100

    Update main.py

--- ml_model/yolo/api/main.py
+++ ml_model/yolo/api/main.py
@@ -32,7 +32,7 @@ async def update_metrics(request, call_next):
     global REQUEST_COUNTER
     global REQUEST_LATENCY
     global WINDOW
-    if str(request.url.path) != '/predict':
+    if str(request.url.path) != '/health':
         response = await call_next(request)
         return response
     
