12199f849374b2249a4a34f62541861cbed84b30
Lahmer Mohammed <lahmermohammed65@gmail.com>
2023-05-29 01:26:07 +0100

    updated run.sh

--- ml_model/yolo/api/run.sh
+++ ml_model/yolo/api/run.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 #uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
-gunicorn main:app -b 0.0.0.0:8000 --workers 1 --threads 1 --worker-class uvicorn.workers.UvicornWorker
+gunicorn main:app -b 0.0.0.0:8000 --workers $1 --threads $2 --worker-class uvicorn.workers.UvicornWorker
